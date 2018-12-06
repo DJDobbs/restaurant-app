ae72ff3c10b9de2d2f0f4f3810a30f4fefc4f6c7
DJD <g>
2018-12-05 19:55:39 -0500

    Add page Orders page. Add buttons next to menu items.

--- restaurant-app/MenuHome.aspx.cs
+++ restaurant-app/MenuHome.aspx.cs
@@ -31,6 +31,7 @@ namespace restaurant_app {
                     sBuilder.Append("<td>" + dRow["Price"].ToString().Trim() + "</td>");
                     sBuilder.Append("<td>" + dRow["Ingredients"].ToString().Trim() + "</td>");
                     sBuilder.Append("<td>" + dRow["Allergens"].ToString().Trim() + "</td>");
+                    sBuilder.Append("<td><button type=\"button\">Add to Cart</button></td>");
                     sBuilder.Append("</tr>");
                 }
                 sBuilder.Append("</table>");
---
+++ restaurant-app/Orders.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Orders.aspx.cs" Inherits="restaurant_app.Orders" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+</asp:Content>
